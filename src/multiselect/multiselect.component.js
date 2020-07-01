48fad177fa50b0454aaf85efd725a429ef86ce85
Srigar Sukumar <srisukum@publicisgroupe.net>
2020-07-01 20:13:02 +0530

    #51 added class to override css in app level

--- src/multiselect/multiselect.component.js
+++ src/multiselect/multiselect.component.js
@@ -415,7 +415,7 @@ export class Multiselect extends React.Component {
     const { inputValue, toggleOptionsList, selectedValues } = this.state;
     const { placeholder, style, singleSelect, id, hidePlaceholder, disable} = this.props;
     return (
-      <div className={`${ms.multiSelectContainer} ${disable ? 'disable' : ''}`} id={id || 'multiselectContainerReact'} style={style['multiselectContainer']}>
+      <div className={`${ms.multiSelectContainer} ${disable ? `${ms.disable_ms} disable_ms` : ''}`} id={id || 'multiselectContainerReact'} style={style['multiselectContainer']}>
         <div className={`${ms.searchWrapper} ${singleSelect ? ms.singleSelect : ''}`} 
           ref={this.searchWrapper} style={style['searchBox']} 
           onClick={singleSelect ? this.toggelOptionList : () => {}}
